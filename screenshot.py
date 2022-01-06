c6bdb3cec02ca9e0a6fd425cc20d1e18ec1dae5a
unknown <adjeidag@gmail.com>
2022-01-06 22:44:29 +0000

    fixed pyautogui error and created a separate folder for catured images

--- screenshot.py
+++ screenshot.py
@@ -1,10 +1,11 @@
 import time
 import pyautogui
 
-# slight issues with pyautogui
+
 def screenshot():
     name = int(round(time.time()*1000))
-    name = "{}.png".format(name)
+    # save file in shots directory
+    name = "C:/Users/JUDE ASMAH/Desktop/screenshot-py/shots/{}.png".format(name) 
     time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
